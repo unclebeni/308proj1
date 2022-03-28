204e511be1f3b0c12d09dd73c9637a6d24e0e5d2
bmoeckly <bmoeckly@co1313-04.ece.iastate.edu>
2022-03-28 12:51:55 -0500

    did some work on appserver

--- BigBank/appserver.c
+++ BigBank/appserver.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <ptrhead.h>
+#include <bank.c>
 
 struct trans
 {
@@ -19,19 +20,46 @@ struct request
 
 struct queue
 {
-	struct request * head, * taile;
+	struct request * head, * tail;
 	int num_jobs;
 }
 
 
 int main(int argc, char** argv)
 {
-	//NEED TO CHECK IF ARGUMENTS ARE NOT NULL FIRST
-	int numThreads = argv[1];
-	int acCount = argv[2];
-	char * outputFile = calloc(sizeof(argv[3], sizeof(argv[3]);
-	strcpy(outputFile, argv[3]);
-	
- 
+	int numThreads;
+	int acCount;
+	char * outputFile;
+	int done = 0;	
+	//CHECK IF ARGUMENTS ARE NOT NULL FIRST
+	if(numThreads != null && acCount != null && argv[3] != null )
+	{
+		numThreads = argv[1];
+		acCount = argv[2];
+		outputfile = calloc(sizeof(argv[3], sizeof(argv[3]));	
+		strcpy(outputFile, argv[3]);
+	}
+	else
+	{
+		printf("Null Argument(s)\n");
+		exit(0);
+	}
 
+	//SET UP ACCOUNTS AND THREADS
+	if(initialize_accounts(acCount) == 0)
+	{
+		printf("Error setting up accounts."\n);
+		exit(0);
+	}
+	for(int i = 0; i < numThreads; i++)
+	{
+		//make new thread.. not sure how to do that currently
+	}
+	
+	do{
+
+	}(while done == 0);
+
+	printf("done");
+		 
 }
