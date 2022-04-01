3fa930fe7a6c48b5419a0d80dbb8c7405c04ce0e
bmoeckly <bmoeckly@vlinux-33.ece.iastate.edu>
2022-04-01 08:56:50 -0500

    should be able to take user input now

--- BigBank/appserver.c
+++ BigBank/appserver.c
@@ -1,9 +1,11 @@
-#include <bank.h>
+
 #include <stdio.h>
 #include <unistd.h>
-#include <ptrhead.h>
-#include <bank.c>
-
+#include <string.h>
+#include <stdlib.h>
+//#include <ptrhead.h>
+#include "Bank.h"
+/*
 struct trans
 {
 	int acc_id;
@@ -23,20 +25,25 @@ struct queue
 	struct request * head, * tail;
 	int num_jobs;
 }
-
+*/
 
 int main(int argc, char** argv)
 {
 	int numThreads;
 	int acCount;
 	char * outputFile;
-	int done = 0;	
+	int done = 0;
+	int k;
+	char * userIn; userIn = (char*) malloc(265);
+	char ** userInBroken; userInBroken = (char**)malloc(10*sizeof(char*));	
+	char * token = "";
+	char delim[1] = " ";
 	//CHECK IF ARGUMENTS ARE NOT NULL FIRST
-	if(numThreads != null && acCount != null && argv[3] != null )
+	if(argv[0] != NULL && argv[2] != NULL && argv[3] != NULL )
 	{
-		numThreads = argv[1];
-		acCount = argv[2];
-		outputfile = calloc(sizeof(argv[3], sizeof(argv[3]));	
+		numThreads = atoi(argv[1]);
+		acCount = atoi(argv[2]);
+		outputFile = (char*)calloc(sizeof(argv[3]), sizeof(argv[3]));	
 		strcpy(outputFile, argv[3]);
 	}
 	else
@@ -48,17 +55,61 @@ int main(int argc, char** argv)
 	//SET UP ACCOUNTS AND THREADS
 	if(initialize_accounts(acCount) == 0)
 	{
-		printf("Error setting up accounts."\n);
+		printf("Error setting up accounts.\n");
 		exit(0);
 	}
-	for(int i = 0; i < numThreads; i++)
-	{
-		//make new thread.. not sure how to do that currently
-	}
 	
-	do{
+	//make threads... not sure how to do that yet
+	
+	do{	//To start, we need to be able to take user input.
+		userInBroken[0] = '\0';
+		printf("ENTER COMMAND> ");
+		fgets(userIn, 256, stdin);
+		if((strlen(userIn) > 0) && (userIn[strlen(userIn)-1] == '\n'))
+		{
+			userIn[strlen(userIn)] = '\0';
+		}
+		k = 0;
+		token = strtok(userIn, delim);
+		while(token != NULL)
+		{
+			userInBroken[k] = calloc(strlen(token), strlen(token));
+			strcpy(userInBroken[k], token);
+			k++;
+			token = strtok(NULL, delim);
+		}		
+		userInBroken[k] = NULL;
+ 		
+		//now we can parse the input and decide how to procede.
+		if(userInBroken[0] == NULL)
+		{ /*do nothing*/ }
+		else if(strcmp(userInBroken[0], "exit") == 0)
+		{done = 1;}
+		else if(strcmp(userInBroken[0], "EXIT") == 0)
+		{done = 1;}
+		else if(strcmp(userInBroken[0], "CHECK") == 0)
+		{	//Check the given account.
+			printf("$d\n", read_account(atoi(userInBroken[0])));
+		}
+		else if(strcmp(userInBroken[1], "BAL") == 0)
+		{
+			printf("Support coming soon.\n");
+		}	
+		else //default
+		{printf("Command not recognized.\n");}
+	
 
-	}(while done == 0);
+	}while(done == 0); 
+
+
+
+	//free all the pointers I allocated space to.
+	free(userIn);
+	int i;
+	for(i = 0; i < sizeof(userInBroken); i++)
+	{
+		free(userInBroken[i]);s
+	}
 
 	printf("done");
 		 
